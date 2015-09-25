91c5149f471cfd4e67eb070daa8eba39746715ed
unknown <clavoie@draftkings.com>
2015-09-24 22:05:58 -0400

    WIP broken

--- src/wedge/core.clj
+++ src/wedge/core.clj
@@ -1,17 +1,21 @@
 (ns wedge.core
-  (:require [clojure.test :as t]))
+  (:require
+   [clojure.test :as t]
+   [wedge.private.core :as private]))
 
-(defn inc-or-1 [v]
-  (if (nil? v) 1 (inc v)))
+;; called-<
+;; called->
+;; called?
 
-(defn add-call [a args]
-  (update-in a (vector args) inc-or-1))
+(defn is-called? [counter args times]
+  (let [fn-called (get counter args 0)]
+    (t/is (= fn-called times))))
 
-(defn is-called? [counter times & args]
-  (t/is (= (get-in @counter args 0) times)))
-
-(defmacro def-stub [counter fn-name & body]
-  `(let [~counter (atom {})
-         origin-fn# ~fn-name]
-     (with-redefs [~fn-name (fn [& args#] (swap! ~counter add-call args#) (apply origin-fn# args#))]
-       ~@body)))
+(defmacro def-stub [bindings & body]
+  (assert (vector? bindings) "The bindings for def-stub must be a vector")
+  (assert (even? (count bindings)) "There must be an even number of forms for def-stub bindings")
+  (let [[fn-name counter] bindings]
+    `(let [~counter (atom {})
+           origin-fn# ~fn-name]
+       (with-redefs [~fn-name (fn [& args#] (swap! ~counter ~private/add-call args#) (apply origin-fn# args#))]
+         ~@body))))
--- test/wedge/core_test.clj
+++ test/wedge/core_test.clj
@@ -2,20 +2,36 @@
   (:require [clojure.test :refer :all]
             [wedge.core :refer :all]))
 
+
 (defn call-a [t]
   (inc t))
 
 (defn call-b [t]
   (call-a t))
 
+(defn call-c
+  ([] 1)
+  ([a] 2))
+
+
+
+(comment
+
+  (deftest is-called?-tests
+  (let [counter (atom {1 {}})]
+    ))
+
+
 (deftest def-stub-not-called-test
   ;; maybe should be def-stub [counter a]
-  (def-stub counter call-a
-    (is-called? counter 0)))
+  (def-stub [call-a counter]
+    (is-called? counter [0] 0)))
 
 (deftest def-stub-called-once-test
-  (def-stub counter call-a
-    (is-called? counter 0 0)
+  (def-stub [call-a counter]
+    (is-called? counter [0] 0)
     (is (= 1 (call-b 0)))
-    (is-called? counter 0)
-    (is-called? counter 1 0)))
+    (println @counter)
+    (is-called? counter [] 0)
+    (is-called? counter [0] 1)))
+)
