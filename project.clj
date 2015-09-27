2a55c56ab4aa77a87bd0391057406819b2b6a24d
Chris LaVoie <clavoie@gmail.com>
2015-09-26 22:29:56 -0400

    v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wedge "1.0.0-SNAPSHOT"
+(defproject wedge "1.0.0"
   :description "Function call verification for use with clojure.test"
   :url "https://github.com/clavoie/wedge"
   :license {:name "Eclipse Public License"
